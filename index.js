903ee53a4c6b9b16114edd658d2f50c35dad7ee0
Ivonaldo Soares <vonaldojrsoares@gmail.com>
2021-09-06 20:30:01 -0300

    adiciona varivel de ambiente

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
+if(process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
+
 const express = require('express');
 const app = express();
 app.use(express.json());
 
 const Conn = require('./models/conn/conn');
 
-Conn("localhost",27017,"musicas");
+const db_url = process.env.DB_URL;
+const db_user = process.env.DB_USER;
+const db_pass = process.env.DB_PASS;
+const db_data = process.env.DB_DATA;
+
+Conn(db_url, db_user, db_pass, db_data);
 
 const port = 3000;
 
--- models/conn/conn.js
+++ models/conn/conn.js
@@ -2,8 +2,10 @@ const mongoose = require('mongoose');
 // url de conexao -> mongodb://servidor:porta/nome do banco
 // useNewUrlParser --> Para que o Moongoose use o novo sistema de Url
 // useUnifiedTopology --> Mecanismo de monitoramento de Banco de Dados
-function Conn(url,porta,banco){
-    mongoose.connect(`mongodb+srv://dbUser:0zXo59QEig7PxcYh@blue-db.etutw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`, {
+function Conn(url, user, pass, banco){
+    mongoose.connect(`${url}/${banco}`, {
+        user: user,
+        pass: pass,
         useNewUrlParser: true,
         useUnifiedTopology: true,
     }).then(() => {
