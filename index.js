d02fcd9e250232d9e6e3df891e3509c84507e534
Ivonaldo Soares <vonaldojrsoares@gmail.com>
2021-09-08 23:11:23 -0300

    update cors

--- index.js
+++ index.js
@@ -3,8 +3,16 @@ if(process.env.NODE_ENV !== 'production') {
 }
 
 const express = require('express');
+const cors = require('cors');
+
+const corsOptions = {
+  origin: 'http://localhost:3001',
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+
 const app = express();
 app.use(express.json());
+app.use(cors(corsOptions));
 
 const Conn = require('./models/conn/conn');
 
@@ -14,8 +22,7 @@ const db_pass = process.env.DB_PASS;
 const db_data = process.env.DB_DATA;
 
 Conn(db_url, db_user, db_pass, db_data);
-
-const port = 3000;
+const port = 3002;
 
 const musicasRouter = require('./routers/musicas.routes');
 app.use('/musicas',musicasRouter);
